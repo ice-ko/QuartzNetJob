f345f0b91a058726c0a0f5322e49673f297e6e4c
K <823166623@qq.com>
2018-04-24 14:46:59 +0800

    修改

--- Module/QuartzNet/QuartzNet2/ScheduleManage.cs
+++ Module/QuartzNet/QuartzNet2/ScheduleManage.cs
@@ -7,45 +7,68 @@ using System.Threading.Tasks;
 
 namespace QuartzNet2.Core
 {
-
-    /// <summary>
-    /// 任务管理
-    /// </summary>
+    /// <summary>任务管理</summary>
     public class ScheduleManage
     {
-        /// <summary>
-        /// 任务计划列表
-        /// </summary>
+        /// <summary>任务计划列表</summary>
         public static List<ScheduleEntity> ScheduleList = new List<ScheduleEntity>();
+        public static IList<ScheduleDetailsEntity> ScheduleDetailList = (IList<ScheduleDetailsEntity>)new List<ScheduleDetailsEntity>();
 
-        public static IList<ScheduleDetailsEntity> ScheduleDetailList = new List<ScheduleDetailsEntity>();
-
-        public virtual ScheduleEntity GetScheduleModel(ScheduleEntity entity)
+        /// <summary>获取任务实例</summary>
+        /// <param name="jobGroup">任务分组</param>
+        /// <param name="jobName">任务名称</param>
+        /// <returns></returns>
+        public virtual ScheduleEntity GetScheduleModel(string jobGroup, string jobName)
         {
-            return ScheduleList.Where(a => a.JobName == entity.JobName && a.JobGroup == entity.JobGroup).FirstOrDefault();
+            return ScheduleManage.ScheduleList.Where<ScheduleEntity>((Func<ScheduleEntity, bool>)(a =>
+            {
+                if (a.JobName == jobName)
+                    return a.JobGroup == jobGroup;
+                return false;
+            })).FirstOrDefault<ScheduleEntity>();
+        }
+
+        public virtual ScheduleEntity RemoveScheduleModel(string jobGroup, string jobName)
+        {
+            ScheduleEntity scheduleModel = this.GetScheduleModel(jobGroup, jobName);
+            if (scheduleModel != null)
+                ScheduleManage.ScheduleList.Remove(scheduleModel);
+            return scheduleModel;
         }
 
         public virtual void UpdateScheduleRunStatus(ScheduleEntity entity)
         {
-            var schedule = ScheduleList.Where(a => a.JobName == entity.JobName && a.JobGroup == entity.JobGroup).FirstOrDefault();
-            schedule.RunStatus = entity.RunStatus;
+            ScheduleManage.ScheduleList.Where<ScheduleEntity>((Func<ScheduleEntity, bool>)(a =>
+            {
+                if (a.JobName == entity.JobName)
+                    return a.JobGroup == entity.JobGroup;
+                return false;
+            })).FirstOrDefault<ScheduleEntity>().RunStatus = entity.RunStatus;
         }
 
         public virtual void AddScheduleDetails(ScheduleDetailsEntity detail)
         {
-            ScheduleDetailList.Add(detail);
+            ScheduleManage.ScheduleDetailList.Add(detail);
         }
 
         public virtual void UpdateScheduleNextTime(ScheduleEntity entity)
         {
-            var schedule = ScheduleList.Where(a => a.JobName == entity.JobName && a.JobGroup == entity.JobGroup).FirstOrDefault();
-            schedule.NextTime = entity.NextTime;
+            ScheduleManage.ScheduleList.Where<ScheduleEntity>((Func<ScheduleEntity, bool>)(a =>
+            {
+                if (a.JobName == entity.JobName)
+                    return a.JobGroup == entity.JobGroup;
+                return false;
+            })).FirstOrDefault<ScheduleEntity>().NextTime = entity.NextTime;
         }
 
         public virtual void UpdateScheduleStatus(ScheduleEntity entity)
         {
-            var schedule = ScheduleList.Where(a => a.JobName == entity.JobName && a.JobGroup == entity.JobGroup).FirstOrDefault();
-            schedule.Status = entity.Status;
+            ScheduleManage.ScheduleList.Where<ScheduleEntity>((Func<ScheduleEntity, bool>)(a =>
+            {
+                if (a.JobName == entity.JobName)
+                    return a.JobGroup == entity.JobGroup;
+                return false;
+            })).FirstOrDefault<ScheduleEntity>().Status = entity.Status;
         }
     }
 }
--- Module/QuartzNet/QuartzNet2/SchedulerCenter.cs
+++ Module/QuartzNet/QuartzNet2/SchedulerCenter.cs
@@ -28,136 +28,119 @@ namespace QuartzNet2.Core
 
         private IScheduler _scheduler;
 
-        /// <summary>
-        /// 返回任务计划（调度机）
-        /// </summary>
+        /// <summary>返回任务计划（调度机）</summary>
         /// <returns></returns>
         private IScheduler Scheduler
         {
             get
             {
-                if (_scheduler != null)
-                    return _scheduler;
-                else
-                {
-                    ISchedulerFactory schedf = new StdSchedulerFactory();
-                    _scheduler = schedf.GetScheduler();
-                    return _scheduler;
-                }
+                if (this._scheduler != null)
+                    return this._scheduler;
+                this._scheduler = new StdSchedulerFactory().GetScheduler();
+                return this._scheduler;
             }
         }
 
-        /// <summary>
-        /// 添加一个工作调度
-        /// </summary>
+        /// <summary>添加一个工作调度</summary>
         /// <param name="m"></param>
         /// <returns></returns>
         private bool AddScheduleJob<T>(ScheduleEntity m) where T : IJob
         {
             try
             {
-                if (m != null)
+                if (m == null)
+                    return false;
+                //开始时间
+                DateTimeOffset startTimeUtc = DateBuilder.NextGivenSecondDate(new DateTimeOffset?((DateTimeOffset)m.StarRunTime), 1);
+                //结束时间
+                DateTimeOffset dateTimeOffset = DateBuilder.NextGivenSecondDate(new DateTimeOffset?((DateTimeOffset)m.EndRunTime), 1);
+                //创建任务
+                var job = JobBuilder.Create<T>().WithIdentity(m.JobName, m.JobGroup).Build();
+                ITrigger trigger = null;
+                //判断触发器类型
+                if (string.IsNullOrEmpty(m.CronStr))
                 {
-                    if (m.StarRunTime == null)
-                    {
-                        m.StarRunTime = DateTime.Now;
-                    }
-
-                    var starRunTime = DateBuilder.NextGivenSecondDate(m.StarRunTime, 1);
-
-                    if (m.EndRunTime == null)
-                    {
-                        m.EndRunTime = DateTime.Now;
-                    }
-                    var endRunTime = DateBuilder.NextGivenSecondDate(m.EndRunTime, 1);
-                    IJobDetail job = job = JobBuilder.Create<T>()
-                        .WithIdentity(m.JobName, m.JobGroup).Build();
-                    ITrigger trigger = null;
-                    if (!string.IsNullOrEmpty(m.CronStr))
-                    {
-                        trigger = (ICronTrigger)TriggerBuilder.Create()
-                            .StartAt(starRunTime)
-                            .EndAt(endRunTime)
-                            .WithIdentity(m.JobName, m.JobGroup)
-                            .WithCronSchedule(m.CronStr)
-                            .Build();
-                    }
-                    else
-                    {
-                        trigger = (ISimpleTrigger)TriggerBuilder.Create().StartAt(starRunTime).WithSimpleSchedule(x => x.WithIntervalInSeconds(1).WithRepeatCount(0)).Build();
-                    }
-                    Scheduler.ScheduleJob(job, trigger);
-                    Scheduler.Start();
-                    StopScheduleJob<ScheduleManage>(m.JobGroup, m.JobName);
-                    return true;
-                }
-                return false;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// 暂停指定的计划
-        /// </summary>
-        /// <param name="jobGroup"></param>
-        /// <param name="jobName"></param>
-        /// <returns></returns>
-        public StatusResult StopScheduleJob<T>(string jobGroup, string jobName) where T : ScheduleManage, new()
-        {
-            try
-            {
-                Scheduler.PauseJob(new JobKey(jobName, jobGroup));
-                new T().GetScheduleModel(new ScheduleEntity
-                {
-                    JobName = jobName,
-                    JobGroup = jobGroup,
-                    Status = EnumType.JobStatus.已停止
-                });
-                return new StatusResult() { Status = 0, Msg = "停止任务计划成功！" };
-            }
-            catch (Exception ex)
-            {
-                return new StatusResult() { Status = -1, Msg = "停止任务计划失败" };
-            }
-        }
-
-        /// <summary>
-        /// 运行指定的计划
-        /// </summary>
-        /// <param name="jobGroup"></param>
-        /// <param name="jobName"></param>
-        /// <returns></returns>
-        public StatusResult RunScheduleJob<T, V>(string jobGroup, string jobName)
-            where T : ScheduleManage, new()
-            where V : IJob
-        {
-            try
-            {
-                var instance = new T();
-                var sm = instance.GetScheduleModel(new ScheduleEntity()
-                {
-                    JobName = jobName,
-                    JobGroup = jobGroup
-                });
-                var result = AddScheduleJob<V>(sm);
-                if (result)
-                {
-                    sm.Status = EnumType.JobStatus.已启用;
-                    instance.UpdateScheduleStatus(sm);
-                    Scheduler.ResumeJob(new JobKey(jobName, jobGroup));
-                    return new StatusResult { Status = 0, Msg = "启动成功" };
+                    trigger = TriggerBuilder.Create().StartAt(startTimeUtc).WithSimpleSchedule(x => x.WithIntervalInSeconds(1).WithRepeatCount(0)).Build();
                 }
                 else
                 {
-                    return new StatusResult { Status = -1, Msg = "启动失败" };
+                    trigger = TriggerBuilder.Create().StartAt(startTimeUtc).EndAt(new DateTimeOffset?(dateTimeOffset))
+                        .WithIdentity(m.JobName, m.JobGroup)
+                        .WithCronSchedule(m.CronStr).Build();
                 }
+                this.Scheduler.ScheduleJob(job, trigger);
+                this.Scheduler.Start();
+                this.StopScheduleJob<ScheduleManage>(m.JobGroup, m.JobName, false);
+                return true;
             }
             catch (Exception ex)
             {
-                return new StatusResult { Status = -1, Msg = "启动失败" };
+                return false;
+            }
+        }
+
+        /// <summary>暂停指定的计划</summary>
+        /// <param name="jobGroup"></param>
+        /// <param name="jobName"></param>
+        /// <param name="isDelete">停止并删除任务</param>
+        /// <returns></returns>
+        public StatusResult StopScheduleJob<T>(string jobGroup, string jobName, bool isDelete = false) where T : ScheduleManage, new()
+        {
+            try
+            {
+                this.Scheduler.PauseJob(new JobKey(jobName, jobGroup));
+                if (isDelete)
+                    Activator.CreateInstance<T>().RemoveScheduleModel(jobGroup, jobName);
+                return new StatusResult()
+                {
+                    Status = 0,
+                    Msg = "停止任务计划成功！"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new StatusResult()
+                {
+                    Status = -1,
+                    Msg = "停止任务计划失败"
+                };
+            }
+        }
+
+        /// <summary>运行指定的计划</summary>
+        /// <param name="jobGroup"></param>
+        /// <param name="jobName"></param>
+        /// <returns></returns>
+        public StatusResult RunScheduleJob<T, V>(string jobGroup, string jobName) where T : ScheduleManage, new() where V : IJob
+        {
+            try
+            {
+                T instance = Activator.CreateInstance<T>();
+                ScheduleEntity scheduleModel = instance.GetScheduleModel(jobGroup, jobName);
+                if (this.AddScheduleJob<V>(scheduleModel))
+                {
+                    scheduleModel.Status = EnumType.JobStatus.已启用;
+                    instance.UpdateScheduleStatus(scheduleModel);
+                    this.Scheduler.ResumeJob(new JobKey(jobName, jobGroup));
+                    return new StatusResult()
+                    {
+                        Status = 0,
+                        Msg = "启动成功"
+                    };
+                }
+                return new StatusResult()
+                {
+                    Status = -1,
+                    Msg = "启动失败"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new StatusResult()
+                {
+                    Status = -1,
+                    Msg = "启动失败"
+                };
             }
         }
     }
